034bcf2261cd4f46d51eaf48c3794ea268f83673
Zuli Dyshekova <z.dyshekova@gmail.com>
2024-09-16 19:18:40 +0300

    Вёрстка карточки категории товаров

--- src/components/category-card.jsx
+++ src/components/category-card.jsx
@@ -6,42 +6,33 @@ import { Color } from '../tokens/colors';
 import { Radius } from '../tokens/radiuses';
 import { Shadow } from '../tokens/shadows';
 
-const StyledCategoryCard = styled.article`
+const StyledWrapper = styled(Link)`
   position: relative;
+  display: grid;
   height: 140px;
-  padding: 0 8px;
   border-radius: ${Radius[4]};
+  background-color: ${Color.GRAY_40};
   overflow: hidden;
+
+  &:hover,
+  &:focus {
+    &::after {
+      content: '';
+      position: absolute;
+      inset: 0;
+      border-radius: ${Radius[4]};
+      box-shadow: ${Shadow.CARD_HOVER_ADULT};
+    }
+  }
 
   @media (min-width: 768px) {
     height: 100px;
   }
 `;
 
-const StyledWrapper = styled(Link)`
-  &:hover,
-  &:focus {
-    ${StyledCategoryCard} {
-      &::after {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        border-radius: ${Radius[4]};
-        box-shadow: ${Shadow.CARD_HOVER_ADULT};
-        pointer-events: none;
-      }
-    }
-  }
-`;
-
-const StyledName = styled(Typography).attrs({
-  as: 'h3',
-  variant: TypographyVariant.TITLE_3
-})`
+const StyledCategoryCard = styled.article`
   position: relative;
+  padding: 0 8px;
 `;
 
 const StyledImage = styled.img`
@@ -53,13 +44,20 @@ const StyledImage = styled.img`
   object-fit: cover;
 `;
 
+const StyledName = styled(Typography).attrs({
+  as: 'h3',
+  variant: TypographyVariant.TITLE_3
+})`
+  position: relative;
+`;
+
 const StyledAmount = styled(Typography).attrs({
   variant: TypographyVariant.TEXT_2
 })`
   position: absolute;
   right: 0;
   bottom: 0;
-  padding: 0 4px 4px;
+  padding: 2px 4px 4px;
   border-radius: ${Radius[4]};
   background-color: ${Color.ORANGE_30};
 `;
@@ -68,7 +66,7 @@ function CategoryCard({ className, name, amount, image, imageAlt }) {
   return (
     <StyledWrapper className={className} to='#'>
       <StyledCategoryCard>
-        <StyledImage src={image} imageAlt={imageAlt} />
+        <StyledImage src={image} alt={imageAlt} />
         <StyledName>{name}</StyledName>
         <StyledAmount>{amount}</StyledAmount>
       </StyledCategoryCard>
